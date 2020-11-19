c48f38627712880ad1fb7541f78ca53d5d143c42
User <User@User-THINK>
2020-11-19 16:14:11 +0530

    This is my first commit

---
+++ nopcommercecucumber-master/src/test/java/testRunner/TestRun.java
@@ -0,0 +1,16 @@
+package testRunner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features={".//Features/"},
+        glue="stepDefinitions",
+        monochrome=true,
+        plugin= {"pretty","html:Reports"}
+)
+public class TestRun {
+}
